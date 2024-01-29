61ad73858cd8ae9a4fa2db5e1a8da6a043ec6817
lambinocarlos <lambinocarlos30@gmail.com>
2024-01-29 16:28:36 +0800

    tiktok table

---
+++ empdash/CMEdit.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="Form.css">
+    <title>Edit Customer Module</title>
+</head>
+<body>
+
+
+    
+
+
+    <div class="container">
+
+        <header>
+        <div>
+          <img src="ampliva.png" alt="Logo" id="logo">
+        </div>
+      </header>
+
+        <form action="#">
+            <div class="form first">
+                <div class="details personal">
+                    <span class="title">Edit Customer Module</span>
+
+                    <div class="fields">
+                        <div class="input-field">
+                            <label>Platform</label>
+                            <input type="text" placeholder="Enter Platform" required>
+                        </div>
+
+                        <div class="input-field">
+                            <label>Brand</label>
+                            <input type="text" placeholder="Enter Brand" required>
+                        </div>
+
+                        <div class="input-field">
+                            <label>Sub-Brand</label>
+                            <input type="text" placeholder="Enter Sub-Brand" required>
+                        </div>
+
+                        <div class="input-field">
+                            <label>Advertiser (1:m)	</label>
+                            <input type="text" placeholder="Enter Advertiser (1:m)" required>
+                        </div>
+
+                        <div class="input-field">
+                            <label>BM Currency	/label>
+                            <input type="text" placeholder="Enter BM Currency" required>
+                        </div>
+
+                        <div class="input-field">
+                            <label>Ad Account Name</label>
+                            <input type="text" placeholder="Enter Ad Account Name" required>
+                        
+                        
+
+                        
+                        </div>
+                    </div>
+                </div>
+
+                <div class="details ID">
+                    
+
+                    <div class="fields">
+                        <div class="input-field">
+                            <label>Ad Account ID</label>
+                            <input type="text" placeholder="Enter Ad Account ID" required>
+                        </div>
+
+                        <div class="input-field">
+                            <label>Margin/Fee</label>
+                            <input type="text" placeholder="Enter Margin/Fee" required>
+                        </div>
+
+                        <div class="input-field">
+                            <label>Credit Line</label>
+                            <input type="text" placeholder="Enter Credit Line" required>
+                        </div>
+
+                        <div class="input-field">
+                            <label>Business Model</label>
+                            <input type="text" placeholder="Enter Business Model" required>
+                        </div>
+
+                        <div class="input-field">
+                            <label>Industry</label>
+                            <input type="text" placeholder="Enter Industry" required>
+                        </div>
+
+                        <div class="input-field">
+                            <label>Agency (1:m)</label>
+                            <input type="text" placeholder="Enter Agency (1:m)" required>
+
+                        </div>
+                    </div>
+
+                    <div class="form first">
+                        <div class="details personal">
+                            
+        
+                            <div class="fields">
+                                <div class="input-field">
+                                    <label>Report Type</label>
+                                    <input type="text" placeholder="Enter Report Type" required>
+                                </div>
+        
+                                <div class="input-field">
+                                    <label>Client Authorized Signatory</label>
+                                    <input type="text" placeholder="Enter Client Authorized Signatory" required>
+                                </div>
+        
+                                <div class="input-field">
+                                    <label>Designation of Client Authorized Signatory</label>
+                                    <input type="text" placeholder="Enter Designation of Client Authorized Signatory" required>
+                                </div>
+        
+                                <div class="input-field">
+                                    <label>Email of Authorized Signatory</label>
+                                    <input type="text" placeholder="Enter Email of Authorized Signatory" required>
+                                </div>
+        
+                                <div class="input-field">
+                                    <label>Contact Number of Authorized Signatory</label>
+                                    <input type="text" placeholder="Enter Contact Number of Authorized Signatory" required>
+                                </div>
+        
+                                <div class="input-field">
+                                    <label>Billing Name (Bill To)</label>
+                                    <input type="text" placeholder="Enter Billing Name (Bill To)" required>
+                                
+                                
+        
+                                
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="details ID">
+                            
+        
+                            <div class="fields">
+                                <div class="input-field">
+                                    <label>Billing Address</label>
+                                    <input type="text" placeholder="Enter Billing Address" required>
+                                </div>
+        
+                                <div class="input-field">
+                                    <label>Billing Contact Person</label>
+                                    <input type="text" placeholder="Enter Billing Contact Person" required>
+                                </div>
+        
+                                <div class="input-field">
+                                    <label>Email Address</label>
+                                    <input type="text" placeholder="Enter Email Address" required>
+                                </div>
+        
+                                <div class="input-field">
+                                    <label>Contact Number</label>
+                                    <input type="text" placeholder="Enter Contact Number" required>
+                                </div>
+        
+                                <div class="input-field">
+                                    <label>Payment Terms</label>
+                                    <input type="text" placeholder="Enter Payment Terms" required>
+                                </div>
+        
+                                <div class="input-field">
+                                    <label>Special Billing Instructions</label>
+                                    <input type="text" placeholder="Enter Special Billing Instructions" required>
+        
+                                </div>
+                            </div>
+
+                            <div class="form first">
+                                <div class="details personal">
+                                   
+                
+                                    <div class="fields">
+                                        <div class="input-field">
+                                            <label>Billing Supporting Documents	</label>
+                                            <input type="text" placeholder="Enter Billing Supporting Documents" required>
+                                        </div>
+                
+                                        <div class="input-field">
+                                            <label>Invoice Soft Copy or Hard Copy?</label>
+                                            <input type="text" placeholder="Enter Invoice Soft Copy or Hard Copy?" required>
+                                        </div>
+                
+                                        <div class="input-field">
+                                            <label>Billing Entity</label>
+                                            <input type="text" placeholder="Enter Billing Entity" required>
+                                        </div>
+                
+                                        <div class="input-field">
+                                            <label>Rebates %</label>
+                                            <input type="text" placeholder="Enter Rebates %" required>
+                                        </div>
+                
+                                        <div class="input-field">
+                                            <label>Adglow Share</label>
+                                            <input type="text" placeholder="Enter Adglow Share" required>
+                                        </div> 
+                
+                                    </div>
+                                </div>
+
+
+
+
+                <button class="nextBtn">
+                        <span class="btnText">Save Changes</span>
+                    </button>
+                
+
+                </div> 
+            </div>
+        </form>
+    </div>
+</body>
+</html>
